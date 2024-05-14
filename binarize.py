fec4ebb8f8b394521d4b5133d7a15c0e4e0a8540
Eduardo Dias <eduvidias@gmail.com>
2024-05-14 20:25:38 -0300

    binarize v1

--- binarize.py
+++ binarize.py
@@ -1,9 +1,36 @@
 import os
 import cv2 as cv
 
-images = os.listdir('./rgb_images/')
-output_path = './binary_images/'
+def get_frame (sec, count):
+    vidcap = cv.VideoCapture('video.mp4')
+    vidcap.set(cv.CAP_PROP_POS_MSEC, sec*1000)
+    has_frames, image = vidcap.read()
+    if has_frames:
+        cv.imwrite("image" + str(count) + ".jpg", image)
+    return has_frames
 
-for img in images:
-    image1 = cv.imread(img)
-    image2 = cv.cvtColor(image1, cv.COLOR_BGR2GRAY)
+
+def save_images():
+    sec = 0
+    count = 1
+    frame_rate = round(1/30, 2) # 30 fps
+    success = get_frame(sec, count)
+    while (success and count<10):
+        count += 1
+        sec += frame_rate
+        sec = round(sec, 2)
+        success = get_frame(sec, count)
+
+
+def binarize():
+    pass
+
+
+def main():
+    save_images()
+    binarize()
+    pass
+
+
+if __name__ == "__main__":
+    main()
