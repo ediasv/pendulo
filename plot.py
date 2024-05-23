c76c2c682ebdbdadf3d04dc10bc4d75cb3fa8c0c
Eduardo Dias <eduardoviniciusdias@alunos.utfpr.edu.br>
2024-05-22 21:35:43 -0300

    finalizando

--- plot.py
+++ plot.py
@@ -15,10 +15,20 @@ plt.plot(x_data, y_data, 'bo')
 
 
 # palpite de parametros
-guess = [0.1, 0.1, 0.04, 2*np.pi, 0]
+guess = [0.12, 0.1, 0, 2*np.pi, 0]
 
 # perform curve fit
 popt, pcov = curve_fit(func, x_data, y_data, guess)
+
+b = popt[2]
+w0 = np.sqrt(popt[3]**2 + b**2)
+m = 0.102
+q = m * w0 / (2 * m * b) # fator de qualidade
+
+f = open('./output/fqualidade.txt', 'w')
+f.write(f'{q}')
+f.close
+
 print(popt)
 
 x_fit = np.arange(0.0, 61, 0.01)
